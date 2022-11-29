a9decc126d2ccb8346fc4e921c4a6873fb3119ce
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-11-29 16:36:53 -0300

    Fix

--- Core/Entities/Producto.cs
+++ Core/Entities/Producto.cs
@@ -16,7 +16,6 @@ namespace Core.Entities
         public Marca Marca { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
-        [Column(TypeName="decimal(18,4)")]
         public decimal precio { get; set; }
         public string imagen { get; set; } 
     }
