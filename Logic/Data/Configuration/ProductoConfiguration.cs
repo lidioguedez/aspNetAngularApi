090d412a199db779cb9d6d9b02d25fd875b74a2f
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-11-29 21:53:22 -0300

    Creando Migraciones

--- Logic/Data/Configuration/ProductoConfiguration.cs
+++ Logic/Data/Configuration/ProductoConfiguration.cs
@@ -16,7 +16,7 @@ namespace Logic.Data.Configuration
             builder.Property(p => p.Nombre).IsRequired().HasMaxLength(250);
             builder.Property(p => p.Descripcion).IsRequired().HasMaxLength(500);
             builder.Property(p => p.imagen).HasMaxLength(1000);
-            builder.Property(p => p.precio).HasColumnType("decimal()18,2");
+            builder.Property(p => p.precio).HasColumnType("decimal(18,2)");
             builder.HasOne(m => m.Marca).WithMany().HasForeignKey(k => k.MarcaId);
             builder.HasOne(c => c.Categoria).WithMany().HasForeignKey(k => k.CategoriaId);
         }
