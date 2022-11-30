8af9f2c9d88ef14fd0c2dbd9864badd2e0f4893e
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-11-29 22:52:41 -0300

    Creando Seeder Marca

---
+++ Logic/Data/Seeders/MarcaDataSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Data.Seeders
+{
+    public static partial class MarcaDataSeeder
+    {
+        public static void MarcaDataSeed(EntityTypeBuilder<Marca> builder)
+        {
+            builder.HasData(
+                new { Id = 1, Nombre = "Marca Generica 1" },
+                new { Id = 2, Nombre = "Marca Generica 2" }
+                );
+        }
+    }
+}
